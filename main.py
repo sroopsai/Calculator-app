531dbdb5152f63f7ae62783dfaad9edec060f3c4
roopsai <roopsai.work@gmail.com>
2019-09-20 15:48:08 +0200

    Division feature and testing for it are added!

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
+@app.route('/div')
+def division():
     value1 = request.args.get('A', default=0, type=int)
     value2 = request.args.get('B', default=0, type=int)
-    result = value1+value2
-    return '%d \n' % result
+    try:
+        result = value1/value2
+    except ZeroDivisionError:
+        return "B's value shouldn't not be zero!"
+    else:
+        return '%.1f \n' % result
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -10,18 +10,13 @@ class TestOnlineCalculator(unittest.TestCase):
         main.app.testing = True
         self.app = main.app.test_client()
 
-    def test_empty_page(self):
-        """Tests page with empty route or no route"""
+    def test_division(self):
+        """Tests page with /div route, testing division feature of the calculator,
+        right now only integers are being tested.
+        //TODO Support Rational Numbers, floats."""
 
-        response_data = self.app.get('/')
-        self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response_data.data)
-
-    def test_addition(self):
-        """Tests page with /add route, testing addition feature of the calculator,
-        right now only integers are being tested."""
-
-        response_data = self.app.get('/add?A=5&B=3')
-        self.assertEqual(b'8 \n', response_data.data)
+        response_data = self.app.get('/div?A=5&B=3')
+        self.assertEqual(b'1.7 \n', response_data.data)
 
 
 if __name__ == '__main__':
