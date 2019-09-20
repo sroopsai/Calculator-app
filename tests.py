2690d7ea771aecdd08ec6e05413349727d509729
roopsai <roopsai.work@gmail.com>
2019-09-20 15:41:30 +0200

    Multiplication feature and required testing for it are added!

--- tests.py
+++ tests.py
@@ -10,18 +10,12 @@ class TestOnlineCalculator(unittest.TestCase):
         main.app.testing = True
         self.app = main.app.test_client()
 
-    def test_empty_page(self):
-        """Tests page with empty route or no route"""
+    def test_multiplication(self):
+        """Tests multiplication feature.,
+        // TODO Support Rational Numbers, fractions. """
 
-        response_data = self.app.get('/')
-        self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response_data.data)
-
-    def test_addition(self):
-        """Tests page with /add route, testing addition feature of the calculator,
-        right now only integers are being tested."""
-
-        response_data = self.app.get('/add?A=5&B=3')
-        self.assertEqual(b'8 \n', response_data.data)
+        response_data = self.app.get('/mul?A=5&B=3')
+        self.assertEqual(b'15 \n', response_data.data)
 
 
 if __name__ == '__main__':
