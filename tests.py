8a3c406cff3e459468687f9f88c28dee6dd624ff
roopsai <roopsai.work@gmail.com>
2019-09-20 15:35:50 +0200

    Subtraction feature added.

--- tests.py
+++ tests.py
@@ -7,21 +7,15 @@ class TestOnlineCalculator(unittest.TestCase):
 
     def setUp(self):
         """Sets up the app for testing"""
+
         main.app.testing = True
         self.app = main.app.test_client()
 
-    def test_empty_page(self):
-        """Tests page with empty route or no route"""
+    def test_subtraction(self):
+        """Tests the subtraction feature -> applies to only integers right now."""
 
-        response_data = self.app.get('/')
-        self.assertEqual(b'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n', response_data.data)
-
-    def test_addition(self):
-        """Tests page with /add route, testing addition feature of the calculator,
-        right now only integers are being tested."""
-
-        response_data = self.app.get('/add?A=5&B=3')
-        self.assertEqual(b'8 \n', response_data.data)
+        response_data = self.app.get('/sub?A=5&B=5')
+        self.assertEqual(b'0 \n', response_data.data)
 
 
 if __name__ == '__main__':
