d6348c35e202078abc8ab7d6007fd4549466a65f
roopsai <roopsai.work@gmail.com>
2019-09-22 21:07:42 +0200

    Tests -> ok!

--- tests.py
+++ tests.py
@@ -17,31 +17,31 @@ class TestOnlineCalculator(unittest.TestCase):
 
         # integer numbers testing
         response_data = self.app.get('/mul?A=5&B=3')
-        self.assertEqual(b'15.0 \n', response_data.data)
+        self.assertEqual(b'15 \n', response_data.data)
 
         # rational numbers testing
         response_data = self.app.get('/mul?A=5/3&B=3/4')
-        self.assertEqual(b'1.2 \n', response_data.data)
+        self.assertEqual(b'1.25 \n', response_data.data)
 
         # when both A and B are both floats
         response_data = self.app.get('/mul?A=5.4&B=3.4678')
-        self.assertEqual(b'18.7 \n', response_data.data)
+        self.assertEqual(b'18.73 \n', response_data.data)
 
         # when A is an int and B is float
         response_data = self.app.get('/mul?A=5&B=-3.4678')
-        self.assertEqual(b'-17.3 \n', response_data.data)
+        self.assertEqual(b'-17.34 \n', response_data.data)
 
         # when A is a float and B is an int
         response_data = self.app.get('/mul?A=-3.4678&B=5')
-        self.assertEqual(b'-17.3 \n', response_data.data)
+        self.assertEqual(b'-17.34 \n', response_data.data)
 
         # when A is a fraction and B is an int
         response_data = self.app.get('/mul?A=3/4&B=5')
-        self.assertEqual(b'3.8 \n', response_data.data)
+        self.assertEqual(b'3.75 \n', response_data.data)
 
         # when A is an int and B is a fraction
         response_data = self.app.get('/mul?A=5&B=3/4')
-        self.assertEqual(b'3.8 \n', response_data.data)
+        self.assertEqual(b'3.75 \n', response_data.data)
 
         # corner cases testing
         # when A = x/0 where x belongs to any integer
